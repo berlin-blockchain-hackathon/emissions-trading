cfbdeb93ed3aaa509b7292ec1a40797e836fd33e
Lorenzo Manacorda <lorenzo@mailbox.org>
2018-03-03 13:34:14 +0100

    Two payment functions

--- contracts/ETS.sol
+++ contracts/ETS.sol
@@ -5,30 +5,55 @@ contract ETS {
         bytes32 name;
         uint allowance;
         uint balance;
-        uint real;
+        uint used;
     }
-    
+
     mapping (address => Firm) public firms;
     mapping (bytes32 => address) public names;
-    
+
     address public creator;
-    
+
+    uint expensivePrice = 10;
+    uint cheapPrice = 2;
+
     function registerFirm(address a, bytes32 name, uint allowance) public {
         require(creator == msg.sender);
         firms[a] = Firm(name, allowance, 0, 0);
         names[name] = a;
     }
-    
+
     function ETS() public {
         creator = msg.sender;
     }
-    
-    function setReal(address a, uint amount) public {
+
+    function setUsed(address a, uint amount) public {
         require(creator == msg.sender);
-        firms[a].real = amount;
+        firms[a].used = amount;
     }
-    
-    //function getFirmByName(bytes32 name) view public returns (address) {
-    //    return names[name];
-    //}
+
+    // should we check that they only buy if used > allowance?
+    function buyCheapCredits() payable public {
+        // TODO: Is there a better way to check for presence in mapping?
+        require(firms[msg.sender].name != 0);
+        
+        require(msg.value <=(firms[msg.sender].allowance-firms[msg.sender].used)*cheapPrice);
+        
+        firms[msg.sender].balance += msg.value / cheapPrice;
+        firms[msg.sender].used += msg.value / cheapPrice;
+    }
+
+    function buyExpensiveCredits() payable public {
+        // TODO: Is there a better way to check for presence in mapping?
+        require(firms[msg.sender].name != 0);
+
+        // require(msg.value <=(firms[msg.sender].allowance-firms[msg.sender].used)*expensivePrice);
+
+        firms[msg.sender].balance += msg.value / expensivePrice;
+        firms[msg.sender].used += msg.value / expensivePrice;
+    }
+
+    // Testing function.
+    function defaultFirm() public {
+        firms[msg.sender] = Firm("foo", 100, 0, 0);
+    }
 }
