8db01cfa4973392a993f19966a85030ff8c83d09
Lorenzo Manacorda <lorenzo@mailbox.org>
2018-03-03 15:50:20 +0100

    add compile.js

---
+++ compile.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const contractPath = path.resolve(__dirname, 'contracts', 'ETS.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
+module.exports = solc.compile(source, 1).contracts[':ETS'];
