b724047e37523d9a26d7449427cc5d89369948f6
wolfganglandes <WolfgangLandes@gmx.de>
2018-03-04 13:47:52 +0100

    Update ETS.sol

--- contracts/ETS.sol
+++ contracts/ETS.sol
@@ -27,8 +27,8 @@ contract ETS {
 
     address public creator;
 
-    uint public expensivePrice = 5 ether;
-    uint public cheapPrice = 2 ether;
+    uint public expensivePrice = 0.3 ether;
+    uint public cheapPrice = 0.1 ether;
     uint public circulation = 1;
     uint public returnPrice = this.balance / circulation;
 
